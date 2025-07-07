5c859affc903ffaeac4bba8693e0ad3d0ced85df
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-07 17:40:27 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -3,17 +3,19 @@ const app = express();
 const port = 8080;
 
 app.get("/register",(req,res)=>{
-    res.send("Get response from server");
+    // res.send("Get response from server");
+    let {user,password}=req.query;
+    res.send(`Standard get request Welcome to the page ${user}`);
     console.log("get request");
 });
 
+app.use(express.urlencoded({extended:true}));
+
 app.post("/register",(req,res)=>{
-    res.send("Post response from server");
-    console.log("post request");
-})
-
-
-
+    let {user,password}=req.body;//body contains all the information about the client has entered
+    console.log(`The post request is ${user}`);
+    res.send(`Welcome ${user}`);
+});
 
 app.listen(port,()=>{
     console.log(`Listining port ${port}`);
